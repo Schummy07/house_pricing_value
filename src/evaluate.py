d2f1ec087d9f43e3dee2e78cdcec1dd43ed0871e
loledo <60259223+loledo@users.noreply.github.com>
2025-07-20 20:48:08 -0300

    adding pipeline steps

--- src/evaluate.py
+++ src/evaluate.py
@@ -3,6 +3,9 @@ from sklearn.metrics import (
     mean_absolute_error,
     mean_absolute_percentage_error
 )
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 
 
 def evaluate_model(model, X_test, y_test):
@@ -17,3 +20,24 @@ def evaluate_model(model, X_test, y_test):
         'MAPE': mean_absolute_percentage_error(y_test, y_pred)
     }
     return metrics
+
+
+def feature_importances(model, X_train, output_dir):
+    """
+    Returns a Plot of feature importances.
+    """
+    importances = model.feature_importances_
+    idx = np.argsort(importances)[::-1]
+    feats = X_train.columns[idx]
+    imps = importances[idx]
+
+    plt.figure(figsize=(12, 8))
+    plt.barh(feats, imps)
+    plt.gca().invert_yaxis()
+    plt.xlabel("Importance")
+    plt.title("Feature Importances")
+    plt.tight_layout()
+
+    fi_path = os.path.join(output_dir, "challenger_model_feature_importance.png")
+    plt.savefig(fi_path)
+    plt.close()
